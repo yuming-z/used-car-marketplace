1a247317d3df5ab8f05cecf6b981c159002c89f5
Andy Zhu <yzhu6876@uni.sydney.edu.au>
2023-09-15 15:29:35 +1000

    Create Order model

--- marketplace/order/models.py
+++ marketplace/order/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+from car.models import Car
+
+class Order(models.Model):
+
+    ORDER_STATUS = [
+        ("PENDING", "PENDING"),
+        ("FINALISED", "FINALISED"),
+    ]
+
+    seller = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="orders")
+    buyer = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="orders")
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name="orders")
+    status = models.CharField(max_length=10, choices=ORDER_STATUS, default="PENDING")
+
+    def clean(self) -> None:
+        if self.seller == self.buyer:
+            raise ValidationError({"buyer": "You cannot buy your own car!"})
