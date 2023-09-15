d4b011853765b5a0729505dd963368f86ab4b44c
Andy Zhu <yzhu6876@uni.sydney.edu.au>
2023-09-15 14:58:10 +1000

    Create models for Car

--- marketplace/car/models.py
+++ marketplace/car/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class CarBrand(models.Model):
+    name = models.CharField(max_length=50)
+
+class ModelBrand(models.Model):
+    name = models.CharField(max_length=50)
+    brand = models.ForeignKey(CarBrand, on_delete=models.CASCADE, related_name="models")
+
+class FuelType(models.Model):
+    name = models.CharField(max_length=50)
+
+class Car(models.Model):
+    year = models.IntegerField()
+    model = models.ForeignKey(ModelBrand, on_delete=models.CASCADE, related_name="cars")
+    registration_no = models.CharField(max_length=50)
+    status = models.CharField(max_length=50)
+    description = models.TextField()
+    odometer = models.IntegerField()
+    price = models.FloatField()
+    condition = models.CharField(max_length=50)
+    fuel_type = models.ForeignKey(FuelType, on_delete=models.CASCADE, related_name="cars")
+    transmission = models.CharField(max_length=50)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="cars")
+    prev_owner_count = models.IntegerField()
+    location = models.CharField(max_length=50)
+
+class CarFile(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name="files")
+    file = models.FileField(upload_to="car/<int:car_id>/uploads/")
--- marketplace/user/models.py
+++ marketplace/user/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from car.models import *
+
 class Wishlist(models.Model):
     user_id = models.OneToOneField(User, on_delete=models.CASCADE)
-    car_id = models.ManyToManyField('car.Car')
+    car_id = models.ManyToManyField(Car)
 
 class Preference(models.Model):
     user_id = models.OneToOneField(User, on_delete=models.CASCADE, related_name="preference")
-    fuel_type = models.ManyToManyField('car.FuelType', blank=True, null=True)
+    fuel_type = models.ManyToManyField(FuelType, blank=True, null=True)
     transmission = models.CharField(max_length=50, blank=True)
     model = models.ManyToManyField('car.CarModel', blank=True, null=True)
-    brand = models.ManyToManyField('car.CarBrand', blank=True, null=True)
+    brand = models.ManyToManyField(CarBrand, blank=True, null=True)
     year_min = models.IntegerField(blank=True, null=True)
     year_max = models.IntegerField(blank=True, null=True)
     odometer_min = models.IntegerField(blank=True, null=True)
