44f8fc3b1ba89dbe09cca0b436c363ffe92b5744
Andy Zhu <yzhu6876@uni.sydney.edu.au>
2023-09-22 14:23:45 +1000

    Test Order model

--- marketplace/marketplace_app/tests/test_model.py
+++ marketplace/marketplace_app/tests/test_model.py
@@ -11,11 +11,31 @@ class OrderTest(TestCase):
         '''
 
         # create a user
-        self.user = Mock(spec=User)
+        self.seller = Mock(spec=User)
+        self.buyer = Mock(spec=User)
 
         # Create a mock for Car
         self.car = Mock(spec=Car)
         
-    def test_order_creation(self):
-        order = Order.objects.create(seller=self.user, buyer=self.user, car=self.car, status="PENDING")
-        self.assertRaises(ValidationError, order.clean)
+    def test_order_same_seller_buyer(self):
+        order = Order.objects.create(seller=self.seller, buyer=self.seller, car=self.car, status="PENDING")
+        self.assertRaises(ValidationError, order.clean, "The buyer and seller cannot be the same person.")
+
+    def test_order_different_seller_buyer(self):
+        order = Order.objects.create(seller=self.seller, buyer=self.buyer, car=self.car, status="PENDING")
+        self.assertIsNone(order.clean(), "The buyer and seller are different persons.")
+    
+    def test_order_invalid_status(self):
+        status = "Test"
+        order = Order.objects.create(seller=self.seller, buyer=self.buyer, car=self.car, status=status)
+        self.assertRaises(ValidationError, order.clean(), "The status is invalid.")
+    
+    def test_order_pending(self):
+        status = "PENDING"
+        order = Order.objects.create(seller=self.seller, buyer=self.buyer, car=self.car, status=status)
+        self.assertIsNone(order.clean(), "The status is valid.")
+
+    def test_order_completed(self):
+        status = "COMPLETED"
+        order = Order.objects.create(seller=self.seller, buyer=self.buyer, car=self.car, status=status)
+        self.assertIsNone(order.clean(), "The status is valid.")
