8448f8007e9332ee53b03cd971a2031cc9e5d0bc
Andy Zhu <yzhu6876@uni.sydney.edu.au>
2023-09-28 23:43:02 +1000

    Test Order model

--- marketplace/marketplace_app/tests/test_model.py
+++ marketplace/marketplace_app/tests/test_model.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
-from unittest.mock import Mock
 
-from ..models import *
+from marketplace_app.models import *
 
 class OrderTest(TestCase):
 
@@ -11,31 +10,43 @@ class OrderTest(TestCase):
         '''
 
         # create a user
-        self.seller = Mock(spec=User)
-        self.buyer = Mock(spec=User)
+        self.seller = User.objects.create(username="seller", password="seller")
+        self.buyer = User.objects.create(username="buyer", password="buyer")
 
-        # Create a mock for Car
-        self.car = Mock(spec=Car)
+        # Create a sample brand for car
+        self.brand = Car_Brand.objects.create(name="Test Brand")
+
+        # Create a test model for car
+        self.model = Car_Model.objects.create(name="Test Model", brand=self.brand)
+        
+        # Create a test fuel
+        fuel = Fuel_Type.objects.create(name="Test Fuel")
+
+        # Create a test transmission
+        transmission = Transmission_Type.objects.create(name="Test Transmission")
+
+        # Create a test car
+        self.car = Car.objects.create(year=2021, model=self.model, registration_number="ABC123", odometer=1000, fuel_type=fuel, status="AVAILABLE", price=1000, description="", condition="EXCELLENT", owner=self.seller, location="Sydney")
+        self.car.transmission.set([transmission])
         
     def test_order_same_seller_buyer(self):
         order = Order.objects.create(seller=self.seller, buyer=self.seller, car=self.car, status="PENDING")
-        self.assertRaises(ValidationError, order.clean, "The buyer and seller cannot be the same person.")
+        self.assertRaises(ValidationError, order.clean)
 
     def test_order_different_seller_buyer(self):
-        order = Order.objects.create(seller=self.seller, buyer=self.buyer, car=self.car, status="PENDING")
-        self.assertIsNone(order.clean(), "The buyer and seller are different persons.")
+        Order.objects.create(seller=self.seller, buyer=self.buyer, car=self.car)
+        self.assertTrue(Order.objects.filter(seller=self.seller, buyer=self.buyer, car=self.car).exists())
+        
     
     def test_order_invalid_status(self):
         status = "Test"
         order = Order.objects.create(seller=self.seller, buyer=self.buyer, car=self.car, status=status)
-        self.assertRaises(ValidationError, order.clean(), "The status is invalid.")
+        self.assertRaises(ValidationError, order.clean_fields)
     
-    def test_order_pending(self):
-        status = "PENDING"
-        order = Order.objects.create(seller=self.seller, buyer=self.buyer, car=self.car, status=status)
-        self.assertIsNone(order.clean(), "The status is valid.")
+    def test_order_status(self):
+        status = ["PENDING", "COMPLETED"]
 
-    def test_order_completed(self):
-        status = "COMPLETED"
-        order = Order.objects.create(seller=self.seller, buyer=self.buyer, car=self.car, status=status)
-        self.assertIsNone(order.clean(), "The status is valid.")
+        for s in range(0, len(status)):
+            with self.subTest(status=status[s]):
+                Order.objects.create(seller=self.seller, buyer=self.buyer, car=self.car, status=status)
+                self.assertTrue(Order.objects.filter(seller=self.seller, buyer=self.buyer, car=self.car, status=status).exists())
