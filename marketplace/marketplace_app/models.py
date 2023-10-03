6b2211471193e90cefad24b6ed09ba63331e4c83
Andy Zhu <yzhu6876@uni.sydney.edu.au>
2023-10-03 13:07:39 +1100

    Change the time input to Django time input

--- marketplace/marketplace_app/models.py
+++ marketplace/marketplace_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
-from datetime import datetime
+from django.utils.timezone import datetime
 
 def validate_year(year):
     """
