5d41d2ee6b116c99716c9279a4735526041372b1
sphu8299 <sphu8299@uni.sydney.edu.au>
2023-10-09 12:12:37 +1100

    linked login.html file; should now be able to access the login page through the home page

--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('marketplace_app.urls')),
+    path('login', include('marketplace_app.urls')),
     path('admin/', admin.site.urls),
 ]
