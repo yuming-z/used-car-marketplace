f843e665b140b435948fd6f5f0fad94813897871
Andy Zhu <yzhu6876@uni.sydney.edu.au>
2023-11-03 11:08:10 +1100

    Add deployment static root

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STTAIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
